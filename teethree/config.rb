d87e843d95718aef79cba66986cecbfff9ee7eb9
GrilloPress <grillopress@gmail.com>
2015-02-23 11:15:05 +0000

    remove asset hash process for development

--- teethree/config.rb
+++ teethree/config.rb
@@ -56,7 +56,7 @@ configure :build do
 
   activate :minify_css
   activate :minify_javascript
-  activate :asset_hash
+  # activate :asset_hash
   activate :relative_assets
   set :relative_links, true
 
