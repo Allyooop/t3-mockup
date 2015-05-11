76906c7573918b4cc34078a4468446487f6989c8
GrilloPress <grillopress@gmail.com>
2015-05-11 10:11:52 +0000

    update spinner.js

--- teethree/source/javascripts/spinner.js
+++ teethree/source/javascripts/spinner.js
@@ -1,7 +1,11 @@
 jquery(document).on('page:fetch', function() {
-  jquery(".loading-indicator").show();
+  //jquery(".loading-indicator").show();
+  jquery("html").css( "cursor", "progress" )
+  return
 });
 
 jquery(document).on('page:change', function() {
-  jquery(".loading-indicator").hide();
+  //jquery(".loading-indicator").hide();
+  jquery("html").css( "cursor", "auto" )
+  return
 });
